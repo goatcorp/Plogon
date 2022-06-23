0fbfa669a71cab1a0b4355bf7c539b8296c22824
goaaats <goatsdev@protonmail.com>
2022-06-24 01:37:05 +0200

    add entrypoint shell script

---
+++ Plogon/static/entrypoint.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Starting build for $PLOGON_PLUGIN_NAME at $PLOGON_PLUGIN_COMMIT"
+
+mkdir /output
+cd /work/repo/$PLOGON_PROJECT_DIR
+dotnet publish -c Release -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
