28fb827cd5c13655a0f3e272c949f4114c943e2f
goat <goatsdev@protonmail.com>
2022-07-30 14:42:51 +0200

    build instead of publish to make DP work

--- Plogon/static/entrypoint.sh
+++ Plogon/static/entrypoint.sh
@@ -4,4 +4,4 @@ echo "Starting build for $PLOGON_PLUGIN_NAME at $PLOGON_PLUGIN_COMMIT"
 
 mkdir /output
 cd /work/repo/$PLOGON_PROJECT_DIR
-dotnet publish -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
+dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
