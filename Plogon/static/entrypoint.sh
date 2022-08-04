25da732a602f470b1b12d10608855d25581fb4e4
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2022-08-04 23:19:38 +0200

    Add method to set version (#8)

--- Plogon/static/entrypoint.sh
+++ Plogon/static/entrypoint.sh
@@ -3,4 +3,8 @@
 echo "Starting build for $PLOGON_PLUGIN_NAME at $PLOGON_PLUGIN_COMMIT"
 
 cd /work/repo/$PLOGON_PROJECT_DIR
-dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
+if [ $PLOGON_PLUGIN_VERSION ]; then
+    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH -p:Version=$PLOGON_PLUGIN_VERSION
+else
+    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
+fi
