e254badc2a518c2fd01600f9a356424d115e5f05
goat <goatsdev@protonmail.com>
2022-11-11 00:25:45 +0100

    specify that this is a plogon build for msbuild

--- Plogon/static/entrypoint.sh
+++ Plogon/static/entrypoint.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-echo "Starting build for $PLOGON_PLUGIN_NAME at $PLOGON_PLUGIN_COMMIT"
+echo "Starting a build for $PLOGON_PLUGIN_NAME at $PLOGON_PLUGIN_COMMIT"
 
 cd "/work/repo/$PLOGON_PROJECT_DIR"
 if [ $PLOGON_PLUGIN_VERSION ]; then
-    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH -p:Version=$PLOGON_PLUGIN_VERSION
+    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH -p:Version=$PLOGON_PLUGIN_VERSION -p:IsPlogonBuild=True
 else
-    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH
+    dotnet build -c Release --source /packages -o /output -p:DalamudLibPath=$DALAMUD_LIB_PATH -p:IsPlogonBuild=True
 fi
