6f5c794c77f8f65e7b9547d372b1bee9f9537132
Helios747 <aserna3@gmail.com>
2023-03-09 15:49:08 -0800

    Add icon check if build succeeds

--- Plogon/BuildProcessor.cs
+++ Plogon/BuildProcessor.cs
@@ -655,7 +655,10 @@ public class BuildProcessor
         var exitCode = containerInspectResponse.State.ExitCode;
 
         Log.Information("Container for build exited, exit code: {Code}", exitCode);
-
+        if (exitCode == 0 && File.Exists(Path.Combine(task.Manifest.Directory.FullName, "images/icon.png")) == false)
+        {
+            throw new Exception("icon.png is missing from images/.");
+        }
         await this.dockerClient.Containers.RemoveContainerAsync(containerCreateResponse.ID,
             new ContainerRemoveParameters
             {
