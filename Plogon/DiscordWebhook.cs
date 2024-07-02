c98902e170b7c399a86bebbdf687c510b8b7aa35
Kaz Wolfe <root@kazwolfe.io>
2024-07-01 21:31:04 -0700

    fix: buffer message sends

--- Plogon/DiscordWebhook.cs
+++ Plogon/DiscordWebhook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using Discord;
@@ -64,5 +65,27 @@ public class DiscordWebhook
         }
 
         return await this.Client.SendMessageAsync(embeds: new[] { embed }, username: username, avatarUrl: avatarUrl);
+    }
+
+    public async Task SendSplitting(Color color, string message, string title, string footer)
+    {
+        var messages = new List<string>();
+
+        var buffer = "";
+        foreach (var part in message.Split("\n"))
+        {
+            if (buffer.Length + part.Length > 2000)
+            {
+                messages.Add(buffer);
+                buffer = "";
+            }
+
+            buffer += part + "\n";
+        }
+
+        foreach (var body in messages)
+        {
+            await this.Send(color, body, title, footer);
+        }
     }
 }
