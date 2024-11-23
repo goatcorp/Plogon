9ac809ea463214f1fadabc4b25b542c06b75baab
goat <16760685+goaaats@users.noreply.github.com>
2024-11-23 01:21:38 +0100

    PAC staffing change

--- Plogon/PlogonSystemDefine.cs
+++ Plogon/PlogonSystemDefine.cs
@@ -17,7 +17,7 @@ public static class PlogonSystemDefine
     [
         "goaaats",
         "reiichi001",
-        "lmcintyre",
+        "perchbirdd",
         "ackwell",
         "karashiiro",
         "philpax",
