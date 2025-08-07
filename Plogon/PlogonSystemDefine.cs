d9898d18b1c7339eebd600987d3f0b9cb974a284
goat <16760685+goaaats@users.noreply.github.com>
2025-08-07 14:38:32 +0200

    API 13

--- Plogon/PlogonSystemDefine.cs
+++ Plogon/PlogonSystemDefine.cs
@@ -8,7 +8,7 @@ public static class PlogonSystemDefine
     /// <summary>
     /// Current API level.
     /// </summary>
-    public const int API_LEVEL = 12;
+    public const int API_LEVEL = 13;
 
     /// <summary>
     /// PAC members, github logins.
